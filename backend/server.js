c2f0a0857d0e1287261de2235fe456e715af0edd
Ignatius Ealias Roy <ignatiusealias22@gmail.com>
2022-09-24 21:08:12 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use((req,res,next) => {
 app.use('/api/workouts',workoutRoutes)
 
 //connect to db
-mongoose.connect(process.env.uri)
+mongoose.connect(process.env.URL)
   .then(()=>{
         //listen for requests
         app.listen(process.env.PORT, () =>{
